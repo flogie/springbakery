2d9b4b9327e3ba05a9c0bba828d92b6179f143f7
floki <flo.gierer@gmail.com>
2021-08-29 21:50:15 +0200

    Initial commit

---
+++ src/main/java/com/example/springbakery/SpringbakeryApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbakery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbakeryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbakeryApplication.class, args);
+    }
+
+}
